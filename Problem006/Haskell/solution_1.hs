badd1f1d90dc8318ccfe8476e8bcf862b45e873b
tkovs <victor.rdg@hotmail.com>
2015-08-25 00:14:24 +0000

    Solution to problem 006, in Haskell

---
+++ Problem006/Haskell/solution_1.hs
@@ -0,0 +1,16 @@
+{-
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural
+numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred
+natural numbers and the square of the sum.
+-}
+
+main :: IO ()
+main = print $ ((sum [1..100]) ^ 2) - (sum $ map (\x -> x * x) [1..100])
