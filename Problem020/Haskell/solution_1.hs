fd3d4ec659107db5a9888d325cd9b5e77cb7a284
tkovs <victor.rdg@hotmail.com>
2015-08-29 22:59:37 +0000

    Solution to problem 020 in Haskell

---
+++ Problem020/Haskell/solution_1.hs
@@ -0,0 +1,12 @@
+{-
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+-}
+
+import Data.Char
+
+main = print $ sum $ map (digitToInt) $ show $ product [1..100]
