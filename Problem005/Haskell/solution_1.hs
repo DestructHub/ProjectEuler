a7cf260fc5d29b3d81a47b5687fb95c95bf51ba9
tkovs <victor.rdg@hotmail.com>
2015-09-02 22:31:06 +0000

    Small improvement in the Haskell solution for problem 005

--- Problem005/Haskell/solution_1.hs
+++ Problem005/Haskell/solution_1.hs
@@ -4,6 +4,6 @@ import Data.List (find)
 import Data.Maybe (fromJust)
 
 smallest :: Int
-smallest = fromJust $ find (\x -> all (\y -> rem x y == 0) [11..20]) [1..]
+smallest = fromJust $ find (\x -> all (\y -> rem x y == 0) [11..20]) [20, 40..]
 
 main = print smallest
