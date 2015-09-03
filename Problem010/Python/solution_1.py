40f20570bb9ec6b15a3b1db810f778ed2861b8ef
Manoel Vilela <manoel_vilela@engineer.com>
2015-09-03 15:07:55 -0300

    Minor fixes on Python Solution at Problem010
    [changes]
    +Python2 interpreter explicit
    
    [comments]
    In this case Python2 it's more fast then Python3.
    Python2 time execution: 4.2s
    Python3 time execution: 6.1s
    
    You can eval with some fixes for actual solution,
    then execute: python2 -m cProfile solution_1.py
    
    Because this I let explicit the python2 interpreter.

--- Problem010/Python/solution_1.py
+++ Problem010/Python/solution_1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 # coding=utf-8
 """
 Summation of primes
@@ -7,6 +8,6 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 Find the sum of all the primes below two million.
 """
-from primes import *
+from primes import primesList
 
 print reduce(lambda x, y: x + y, primesList(2000000))
