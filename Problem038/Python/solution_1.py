d283df53815aece814c63da418053e8b70c61e6b
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-16 03:29:59 -0300

    Add Python solution for Problem038 @0.7s
    The trick is get the max num possible, some greater or equal 10000
    don't makes sense because the pandigital needs mapped between (1, 2)
    at least. Pandigital is 9-digit number. 10000 has 5 digits, map
    to (1, 2), so the concated number will have 10 digits.
    
    We need only check n < 10000 and then we done.

---
+++ Problem038/Python/solution_1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+#   Python Script
+#
+#   Copyright © Manoel Vilela
+#
+#
+
+from collections import Counter
+
+
+class PanDigital(int):
+
+    dig = ''
+    iteration = 1
+
+    def map(self, x):
+        return str(self * x)
+
+    def concat(self, digits):
+        return self.dig + digits
+
+    @property
+    def can_continue(self):
+        return len(self.dig) < 10 and self.count
+
+    @property
+    def count(self):
+        return all(False for x in Counter(self.dig).values() if x > 1)
+
+    @property
+    def sorted(self):
+        return ''.join(map(str, sorted(map(int, self.dig))))
+
+    @property
+    def ispandig(self):
+        return self.sorted == '123456789'
+
+    def transform(self):
+        while self.can_continue:
+            self.dig = self.concat(self.map(self.iteration))
+            if self.ispandig:
+                return self.dig
+            self.iteration += 1
+
+        return None
+
+    @property
+    def value(self):
+        v = self.transform()
+        if v is not None:
+            return int(v)
+        return None
+
+p = PanDigital
+print(max(p(x).value for x in range(10000) if p(x).value))
