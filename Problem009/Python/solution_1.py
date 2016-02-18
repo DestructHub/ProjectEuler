24359656b910ef0bdca455c5d9a5a26a59feeee0
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-17 21:24:18 -0300

    [review] Add python3 compatibility at Problem009
    Remove print stdout useless stuff and print only the answer
    covers problem #2.
    
    By the way... that solutions is a little more slowly. 100 mseconds.
    The older runs with python2 in my machine at @0.2s now @0.3s.
    Python3 for numeric computation is 2x more slow, @0.6s.

--- Problem009/Python/solution_1.py
+++ Problem009/Python/solution_1.py
@@ -1,4 +1,12 @@
-#coding=utf-8
+#!/usr/bin/env python
+# coding=utf-8
+#   Python Script
+#
+#   Copyleft © Manoel Vilela
+#
+#
+
+from __future__ import print_function
 """
 Special Pythagorean triplet
 Problem 9
@@ -13,33 +21,25 @@ Find the product abc.
 
 """
 
+
 def decompSum(n):
-	maxRange = n // 2 # i want to break free (queen)
-	#from euclids formula, its possible proof the a and b its multiples to 3-4 and c multiple 5
-	range_a_or_b = filter(lambda x: x % 3 == 0 or x % 4 == 0 or x % 5 == 0, range(1, maxRange))
-	range_c = filter(lambda x: x % 5 == 0, range_a_or_b)
-	for a in range_a_or_b:
-		for b in range_a_or_b:
-			for c in range_c:
-				if a + b + c == n and a != b != c:
-					yield sorted((a, b, c))
+    from itertools import combinations
+    m = (x for x in range(1, n // 2))
+    div = [3, 4, 5]
+    comb = combinations((x for x in m if any(d for d in div if not x % d)), 3)
+    for a, b, c in comb:
+        if a + b + c == n and a != b != c:
+            yield sorted((a, b, c))
+
 
 def pythagorean(a, b, c):
-	return (a ** 2 + b ** 2) == c ** 2
+    return (a ** 2 + b ** 2) == c ** 2
+
 
 def problem9(n):
-	for a, b, c in decompSum(n):
-		if pythagorean(a, b, c):
-			print '%d² + %d² = %d² (%d)' %(a, b, c, a + b + c)
-			print '%d * %d * %d = %d' %(a, b, c, a*b*c)
-			return	
+    for a, b, c in decompSum(n):
+        if pythagorean(a, b, c):
+            return a * b * c
 
-problem9(1000)
 
-# out:
-#200² + 375² = 425² (1000)
-#200 * 375 * 425 = 31875000
-
-# out:
-#200² + 375² = 425² (1000)
-#200 * 375 * 425 = 31875000
+print(problem9(1000))
