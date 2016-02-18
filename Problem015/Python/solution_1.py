9f98dd9894f21b7281137c66a209abb9c889b693
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-17 22:19:03 -0300

    [stdout] review at solution of Problem015

--- Problem015/Python/solution_1.py
+++ Problem015/Python/solution_1.py
@@ -1,32 +1,46 @@
-#coding=utf-8
+#!/usr/bin/env python
+# coding=utf-8
+#   Python Script
+#
+#   Copyleft © Manoel Vilela
+#
+#
+
 """
 Lattice paths
 Problem 15
 
-Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+Starting in the top left corner of a 2×2 grid,
+and only being able to move to the right and down,
+here are exactly 6 routes to the bottom right corner.
 
 How many such routes are there through a 20×20 grid?
 """
-#answer: 137846528820
-#c(40,20) = 40!/20!20!
+
+# answer: 137846528820
+# c(40,20) = 40!/20!20!
+
+
+# Crazy Pascal Way
 def pascal(n):
-	line = [1]
-	lines = 0
-	while lines < n:
-		i = 0
-		newline = [1, 1]
-		while len(newline) -1 <= lines and len(line) - 1 > i:
-			new = line[i] + line[i + 1] 
-			newline.insert(i + 1, new)
-			i += 1
-		line = newline
-		lines += 1
-		#yield ' '.join([str(x) + ' ' for x in line])
-	return line
-		
-p = pascal(20)
-for x in p:
-	print x
+    line = [1]
+    lines = 0
+    while lines < n:
+        i = 0
+        newline = [1, 1]
+        while len(newline) - 1 <= lines and len(line) - 1 > i:
+            new = line[i] + line[i + 1]
+            newline.insert(i + 1, new)
+            i += 1
+        line = newline
+        lines += 1
+
+    return line
 
 
+def solution():
+    pascal_line = pascal(40)
+    middle = len(pascal_line) // 2
+    return pascal_line[middle]
 
+print(solution())
