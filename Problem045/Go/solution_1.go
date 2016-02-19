6e822fba176bc341f962348a277c20ee02505cd1
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-19 04:27:24 -0300

    [P045-Go] some simplifcations avoiding useless jumps, @luizperes advice.

--- Problem045/Go/solution_1.go
+++ Problem045/Go/solution_1.go
@@ -11,25 +11,21 @@ import (
 	"math"
 )
 
-type polynom struct {
+type Polynom struct {
 	a, b, c float64
 }
 
-var P = polynom{3, -1, -2}
-var H = polynom{2, -1, -1}
-
-func quadratic(a, b, c float64) float64 {
-	return (-b + math.Sqrt(b*b-4*a*c)) / (2 * a)
-}
+var P = Polynom{a: 3, c: 2}
+var H = Polynom{a: 2, c: 1}
+var B float64 = 1
 
 func main() {
 	lastTriangle := 285.0
-
 	for {
 		lastTriangle += 1
 		t := (math.Pow(lastTriangle, 2) + lastTriangle) / 2
-		p := quadratic(P.a, P.b, t*P.c)
-		h := quadratic(H.a, H.b, t*H.c)
+		p := (B + math.Sqrt(B+4*P.a*P.c*t)) / (2 * P.a)
+		h := (B + math.Sqrt(B+4*H.a*H.c*t)) / (2 * H.a)
 
 		if math.Floor(p) == p && math.Floor(h) == h {
 			fmt.Printf("%.0f\n", t)
