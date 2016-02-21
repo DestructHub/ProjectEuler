aec939b4d7503f6cc0e47d69d1241309b77f92c2
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-21 06:23:32 -0300

    Add Go solution for Problem001

---
+++ Problem001/Go/solution_1.go
@@ -0,0 +1,17 @@
+// Copyright 2016 the <project> Authors. All rights reserved
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	fmt.Println(sum)
+}
