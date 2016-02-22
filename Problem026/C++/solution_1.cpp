7bbf92903960af0448582ec0dacae54395521ed4
Manoel Vilela <manoel_vilela@engineer.com>
2016-02-22 04:42:34 -0300

    [P026] Answer wrong at C++ solution, fixed.
    The question asking about the 'd' number for each 1/d between [1, 1000]
    whose recurring cycle is max, not the max numbers of recurring cycles.

--- Problem026/C++/solution_1.cpp
+++ Problem026/C++/solution_1.cpp
@@ -2,37 +2,41 @@
 #include <string.h>
 #define max(x,y) (x)>(y)?(x):(y)
 #define D 1000
- 
-int remainders[D+2];
- 
-int get_max_rec( int n )
+
+int remainders[D];
+
+int get_max_rec(int n)
 {
-    int time=1,dividend=1;
-    memset(remainders,0,(D+2)*sizeof(int));
+    int times = 1,dividend = 1;
+    memset(remainders, 0, D * sizeof(int));
     //printf("-----------------------------\n");
     //printf("%d\n",n);
     //printf("-----------------------------\n");
     for(; dividend!=0 ;)
-    {   
-        //printf("%d %d\n",dividend,time);  
-        if( remainders[dividend] )
-            return time - remainders[dividend];
-        remainders[dividend] = time++;
-        if(dividend<n)
-            dividend*=10;
-        dividend = dividend%n;
+    {
+        //printf("%d %d\n",dividend,times);
+        if (remainders[dividend])
+            return times - remainders[dividend];
+        remainders[dividend] = times++;
+        if (dividend < n)
+            dividend *= 10;
+        dividend = dividend % n;
     }
     return 0;
 }
- 
-int main()
+
+int main(int argc, char *argv[])
 {
-    int max_rec = 0,d;
-    for( d=2 ; d<D ; d++ )
+    int max_rec = 0, d, d_max;
+    for( d = 3 ; d < D ; d++)
     {
-        //printf( "%d : %d\n",d , get_max_rec(d) );
-        max_rec = max( get_max_rec(d) , max_rec );
+        int new_max_rec = get_max_rec(d);
+        //printf( "%d : %d\n",d , new_max_rec(d) );
+        if (new_max_rec > max_rec) {
+            max_rec = new_max_rec;
+            d_max = d;
+        }
     }
-    printf("%d\n",max_rec);
+    printf("%d\n", d_max);
     return 0;
 }
