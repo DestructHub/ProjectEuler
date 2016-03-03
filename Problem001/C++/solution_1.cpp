92ed09beacf6ba8a36394576fc30b6ad4bec9b9f
Rafael Campos Nunes <rafaelnunes@engineer.com>
2016-03-02 21:48:44 -0300

    A naive approach to the problem, however, it solves in ~1 sec.
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ Problem001/C++/solution_1.cpp
@@ -0,0 +1,35 @@
+/*
+ * Find the sum of all multiples of 3 and 5 below 1000 
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+    // upper default limit.
+    const char *limit = "1000";
+    
+    if (argv[1]) {
+	limit = argv[1];
+    }
+    
+    // iterator variable.
+    int i = 0;
+    // resultant sum of all multiples of 3 and 5.
+    int sum = 0;
+    // transform an array of characters into an integer.
+    int number_to = atoi(limit);
+	
+    while (i < number_to) {
+	if ((i % 3 == 0) && (i % 15 != 0)) {
+	    sum += i;
+	}
+ 	if ((i % 5 == 0)) {
+	    sum += i;
+	}
+	i++;
+    }
+    
+    std::cout << sum << std::endl;
+}
