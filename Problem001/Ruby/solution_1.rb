a82732efc10a2928d960b1a04f8010c277432524
tkovs <victor.rdg@hotmail.com>
2016-03-10 00:03:38 -0300

    Cleaner ruby code

--- Problem001/Ruby/solution_1.rb
+++ Problem001/Ruby/solution_1.rb
@@ -4,4 +4,4 @@ def solve
 	(0..999).select{|n| n % 3 == 0 || n % 5 == 0}.inject(:+)
 end
 
-puts solve
+puts(solve)
--- Problem009/Ruby/solution_1.rb
+++ Problem009/Ruby/solution_1.rb
@@ -1,12 +1,15 @@
-def test(a,b,c)
-    return(false) unless (a*a + b*b == c*c)
-    return(false) unless (a + b + c == 1000)
+#Author: tkovs
+
+def test(a, b, c)
+    return false unless (a*a + b*b == c*c)
+    return false unless (a + b + c == 1000)
 
     return true
 end
 
 def solve
-    (1..444).each{|a| (1..444).each{|b| (1..444).each{|c| return (a*b*c) if (test(a,b,c))}}}
+    x = (1..444)
+    x.each{|a| x.each{|b| x.each{|c| return (a * b * c) if (test(a, b, c))}}}
 end
 
-puts(solve)
+puts solve
