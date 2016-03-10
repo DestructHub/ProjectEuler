b47e5b69bc472bf5d0154615de5dcf41c70f8b6d
Manoel Vilela <manoel_vilela@engineer.com>
2016-03-10 16:36:22 -0300

    [P010][C++] Again... disable primes printing and linting, @rafaelcn~
    The most blame code of the earth.
    
    * add space between operators
    * tab or spaces, never mixed; i addopt use of 4 spaces
    * delete trailing whitespaces
    * HMMM, because the creator of languages allows {} breaking lines
      i let this, but I and my linter don't agreed with that.

--- Problem010/C++/solution_1.cpp
+++ Problem010/C++/solution_1.cpp
@@ -6,51 +6,50 @@
 #include <cstdlib>
 
 bool is_prime(unsigned int number);
- 
+
 int main(int argc, char* argv[])
 {
     const char *num = "2000000";
-    
+
     if (argv[1]) {
-	num = argv[1];
+        num = argv[1];
     }
-    
+
     // iteration variables.
     int i = 1;
     // the number to reach.
     int number_to = std::atoi(num);
-    
+
     long long int sum = 0;
-    
+
     while (i < number_to) {
-	if (is_prime(i)) {
-	    std::cerr << i << std::endl;		
-	    sum += i;
-	}
-	i++;
+        if (is_prime(i)) {
+            sum += i;
+        }
+        i++;
     }
-    
+
     std::cout << sum << std::endl;
 }
- 
+
 bool is_prime(unsigned int number) 
 { 
-  if (number <= 1) { 
-      return false;
-  }
- 
-  unsigned int i;
-  for (i=2; i*i<=number; i++) {
-      if (number % i == 0) {
-	  return false;
-      }
-  }
+    if (number <= 1) {
+        return false;
+    }
 
-  // naive solution
-  /*for (i = 2; i < 10; i++) {
-      if (number % i == 0)
-	  return false;
-  }*/
-  
-  return true;
+    unsigned int i;
+    for (i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    // naive solution
+    /*for (i = 2; i < 10; i++) {
+        if (number % i == 0)
+            return false;
+    }*/
+
+    return true;
 }
