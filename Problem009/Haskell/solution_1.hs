b23ffd30e8a33e7535d9b7ce736950bfe991c5a5
tkovs <victor.rdg@hotmail.com>
2016-03-10 00:39:01 -0300

    [P009] [Ruby,Haskell] No longer needs to sum=1000 verification

--- Problem009/Haskell/solution_1.hs
+++ Problem009/Haskell/solution_1.hs
@@ -1,6 +1,6 @@
 -- Author: G4BB3R
 -- Help: tkovs
 
-triangulo = head [(a, b, c) | a <- [1..444], b <- [a..444], c <- [1000 - b - a], a ^ 2 + b ^ 2 == c ^ 2, a + b + c == 1000]
+triangulo = head [(a, b, c) | a <- [1..444], b <- [a..444], c <- [1000 - b - a], a ^ 2 + b ^ 2 == c ^ 2]
 
 main = print $ let (a, b, c) = triangulo in a * b * c
--- Problem009/Ruby/solution_1.rb
+++ Problem009/Ruby/solution_1.rb
@@ -1,13 +1,8 @@
-def test(a, b, c)
-    return false unless (a*a + b*b == c*c)
-    return false unless (a + b + c == 1000)
-
-    return true
-end
+#Author: tkovs
 
 def solve
     x = (1..444)
-    x.each{|a| x.each{|b| [1000 - b - a].each{|c| return (a * b * c) if (test(a, b, c))}}}
+    x.each{|a| x.each{|b| [1000 - b - a].each{|c| return (a * b * c) if (a*a + b*b == c*c)}}}
 end
 
 puts(solve)
