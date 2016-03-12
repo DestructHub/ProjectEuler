12a79a892790a3784037064d8df0d8d635fe1704
Manoel Vilela <manoel_vilela@engineer.com>
2016-03-12 10:28:38 -0300

    [P034] python solution tagged as slow because takes almost 90s to finish

--- Problem034/Python/solution_slow_1.py
+++ Problem034/Python/solution_slow_1.py
@@ -22,10 +22,10 @@ Find the sum of all numbers which are equal to the sum of the factorial of their
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 """
 
-f = lambda n: reduce(lambda a, b: a*b, [u for u in range(n + 1) if u != 0] + [1])
+f = lambda n: reduce(lambda a, b: a * b, [u for u in range(n + 1) if u != 0] + [1])
 c = lambda x: sum([f(int(n)) for n in str(x)]) == x
 filtered_range = lambda i: filter(lambda x: not bool(sum([int(n) for n in str(x)]) & x), range(3, i)) 
 gen = lambda y: (x for x in filtered_range(y) if c(x))
-g = gen(10 ** 7) #because 9!*7 < 99999999
+g = gen(10 ** 7) # because 9!*7 < 99999999
 s = sum(x for x in g)
 print(s)
