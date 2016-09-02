709169464564cd062cb8b359ced22fef890cabbc
Gabriel Torrecillas Sartori <gabriel.sartori04@gmail.com>
2016-09-02 18:22:49 -0300

    Update solution_2.hs

--- Problem003/Haskell/solution_2.hs
+++ Problem003/Haskell/solution_2.hs
@@ -1,9 +1,7 @@
-import Data.Function (fix)
-
-prime f value acc
+prime value acc
     | acc >= value = acc
-    | mod value acc == 0 = f (div value acc) (acc + 1)
-    | otherwise = f value (acc + 1)
+    | mod value acc == 0 = prime (div value acc) (acc + 1)
+    | otherwise = prime value (acc + 1)
 
 main :: IO ()
-main = print $ fix prime 600851475143 2
+main = print $ prime 600851475143 2
