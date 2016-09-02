ecd2ef532d7d42715ca6795c7641ab940a44457e
Leonardo <leo.rog@gmail.com>
2016-09-02 15:12:20 -0300

    Solve 25 in Clojure

---
+++ Problem025/Clojure/solution_1.clj
@@ -0,0 +1,10 @@
+(defn fib-seq
+  ([] (fib-seq (bigint 0) (bigint 1)))
+  ([a b] (lazy-seq (cons b (fib-seq b (+ a b))))))
+
+(defn solution
+  []
+  (->> (fib-seq)
+       (take-while #(< (count (str %)) 1000))
+       (count)
+       (+ 1)))
