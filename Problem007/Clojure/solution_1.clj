594bdfc83f9dfc6a3bf962e03365b75cf6f7d88f
Leonardo <leo.rog@gmail.com>
2016-09-04 14:34:26 -0300

    Solve 007 in Clojure

---
+++ Problem007/Clojure/solution_1.clj
@@ -0,0 +1,22 @@
+(defn is-prime
+  [n]
+  (if (or (= n 1) (= n 4))
+    false
+    (loop [candidates (range 2 (+ 1 (Math/sqrt n)))]
+      (cond
+        (empty? candidates) true
+        (= 0 (rem n (first candidates))) false
+        :else (recur (next candidates))))))
+
+(defn next-prime
+  [n]
+  (loop [candidate n]
+    (if (is-prime candidate)
+      candidate
+      (recur (+ candidate 1)))))
+
+(defn prime-seq
+  ([] (prime-seq 2))
+  ([n] (cons n (lazy-seq (prime-seq (next-prime (+' 1 n )))))))
+
+(str (last (take 10001 (prime-seq))))
