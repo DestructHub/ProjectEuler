29554957da6adc8b927712419b0ca7e8fc0813e9
Leonardo <leo.rog@gmail.com>
2016-09-04 14:24:53 -0300

    prime seq fix

--- Problem003/Clojure/solution_1.clj
+++ Problem003/Clojure/solution_1.clj
@@ -2,7 +2,7 @@
   [n]
   (if (or (= n 1) (= n 4))
     false
-    (loop [candidates (range 2 (Math/sqrt n))]
+    (loop [candidates (range 2 (+ 1 (Math/sqrt n)))]
       (cond
         (empty? candidates) true
         (= 0 (rem n (first candidates))) false
@@ -10,14 +10,14 @@
 
 (defn next-prime
   [n]
-  (loop [candidate (+' n 1)]
+  (loop [candidate n]
     (if (is-prime candidate)
       candidate
-      (recur (+' candidate 1)))))
+      (recur (+ candidate 1)))))
 
 (defn prime-seq
   ([] (prime-seq 2))
-  ([n] (cons n (lazy-seq (prime-seq (next-prime n))))))
+  ([n] (cons n (lazy-seq (prime-seq (next-prime (+' 1 n )))))))
 
 (defn solution
   [n]
