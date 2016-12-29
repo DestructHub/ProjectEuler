8d347b9d3688aec8e3d89883ce487cc1ba191015
Vidal Vasconcelos <vidalvasconcelos@live.com>
2016-12-29 19:30:38 -0300

    PHP solution for this problem.
    If there was some kind of compression of lists in php, it would be easier.

---
+++ Problem002/PHP/solution_1.php
@@ -0,0 +1,16 @@
+<?php
+
+    $fibonacci = [1, 2];
+
+    array_walk( $fibonacci, function($num) use (&$fibonacci)
+    {
+        if (4000000 / $num > 2.6244){ // Will never be smaller then Φ²
+            array_push( $fibonacci, $num + next($fibonacci));
+            prev($fibonacci);
+        }
+    });
+    
+    echo array_sum($fibonacci);
+    
+?>
+
