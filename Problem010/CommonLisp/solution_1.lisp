f76259bc14945273de1e1da666c4949ee9fd1a45
Manoel Vilela <manoel_vilela@engineer.com>
2017-02-04 09:49:59 -0300

    Brief improvement on Common Lisp solution for Problem010
    I sum directly the prime number 2 before so I just need check for
    odd numbers later.

--- Problem010/CommonLisp/solution_1.lisp
+++ Problem010/CommonLisp/solution_1.lisp
@@ -2,19 +2,17 @@
 ;; Manoel Vilela
 
 (defun primep (n)
-  (let ((divisors (loop for x from 2 to (floor (1+ (sqrt n)))
-                        collect x)))
-    (loop for d in divisors
-          never (and (= (mod n d) 0)
-                     (not (= n d))))))
+  (loop for d from 2 to (floor (1+ (sqrt n)))
+        never (and (= (mod n d) 0)
+                   (not (= n d)))))
 
 
 (defun solution (limit)
-  (loop for x from 2
-        for prime? = (primep x)
-        count prime? into primes
-        while (< x limit)
-        when prime? sum x))
+  (+ 2 (loop for x from 3 by 2
+             for prime? = (primep x)
+             count prime? into primes
+             while (< x limit)
+             when prime? sum x)))
 
 
 (format t "~a~%" (solution 2000000))
