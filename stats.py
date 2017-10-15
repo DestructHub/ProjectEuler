96de3d9d13417be46c0a4c5a4a98fd206b3b18e1
a-hilaly <hilalyamine@gmail.com>
2017-10-15 21:46:23 +0200

    Digest answer clean_answer

--- stats.py
+++ stats.py
@@ -324,8 +324,7 @@ def digest_answer(answer):
     # This would be can fixed using `printf` instead `echo`.
     # But things need be rebuild from start. YES. Recreate all the hashes.
     # SO NO NO NO NOO NOOOOOOOOO
-    hacky = clean_answer # the hacky works
-    return hashlib.md5(hacky.encode('utf-8')).hexdigest()
+    return hashlib.md5(clean_answer.encode('utf-8')).hexdigest()
 
 
 def search_language(query, languages):
