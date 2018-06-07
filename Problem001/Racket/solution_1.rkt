bcf41e1f6c3fd0351403ba0186139088fbf8cafb
Manoel Vilela <manoel_vilela@engineer.com>
2018-06-07 02:36:59 -0300

    Fix Racket solution
    For some reason with #lang racket on top of file, executing this file
    as script by `racket --script` doesn't print anything.
    
    This is a quite weird.

--- Problem001/Racket/solution_1.rkt
+++ Problem001/Racket/solution_1.rkt
@@ -1,11 +1,10 @@
-#lang racket
-
 (define (mult3-5 x)
   (or (= (remainder x 3) 0)
       (= (remainder x 5) 0)))
 
 (define (main)
   (let ((answer (apply + (filter mult3-5 (range 0 1000)))))
-    (display (format "~a~%" answer))))
+    (display answer)
+    (newline)))
 
 (main)
