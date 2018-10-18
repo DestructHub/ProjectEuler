256dd233cf532c83f4c66494789b6ce32cf4d0b0
Manoel Vilela <manoel_vilela@engineer.com>
2018-10-18 20:50:39 -0300

    Send answer to stdout in Problem002/Elixir/solution_1.ex

--- Problem002/Elixir/solution_1.ex
+++ Problem002/Elixir/solution_1.ex
@@ -1,3 +1,4 @@
 Stream.unfold({0, 1}, fn {a, b} -> {a, {b, a + b}} end)
 |> Stream.filter(&(rem(&1, 2) == 0))
 |> Enum.reduce_while(0, fn (i, acc) -> if (i < 4_000_000), do: {:cont, acc + i}, else: {:halt, acc} end)
+|> IO.puts
