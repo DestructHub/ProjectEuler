3e0f9db2c1f382d4bf9d1afbbe3f358ad9e166e8
Naman Gera <43007653+namannimmo10@users.noreply.github.com>
2019-10-01 18:44:58 +0530

    Problem 2: new solution in c++ added.

---
+++ Problem002/C++/solution_2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool odd (int i) {
+   return i & 1;
+}
+int main () {
+   vector<int> fib(30);
+   fib[0] = 1;
+   adjacent_difference (fib.begin(), fib.end()-1,fib.begin()+1,
+                  std :: plus<int>());   
+   fib.erase (std :: remove_if (fib.begin(), fib.end(), odd),
+                     fib.end());
+   //clock_t end(clock() - start);
+   std :: cout << accumulate (fib.begin(), fib.end(),
+                     0);
+   return 0;
+}
