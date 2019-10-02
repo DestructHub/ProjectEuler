09253978e0336733fa7cb21d90e1a0542c1ba84f
Naman Gera <43007653+namannimmo10@users.noreply.github.com>
2019-10-02 14:44:38 +0530

    Update solution_1.cpp

--- Problem024/C++/solution_1.cpp
+++ Problem024/C++/solution_1.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(){
   char numbers[] = "0123456789";
-  for(int i=1;i<1000000;i++)
+  for(int i=1;i<1000000;i++){
     next_permutation(numbers,numbers+10); 
+  }
   cout << numbers << endl;
 }
