f3b8b9e1fd896e7ba9fde9037e45071e1c45f5ca
Naman Gera <43007653+namannimmo10@users.noreply.github.com>
2019-10-02 14:48:17 +0530

    Update solution_1.java

--- Problem001/Java/solution_1.java
+++ Problem001/Java/solution_1.java
@@ -5,8 +5,9 @@ public class Main
 		int total = 0;
 		for(int i = 1; i < 1000; i ++)
 		{
-			if(i % 3 == 0 || i % 5 == 0)
+			if(i % 3 == 0 || i % 5 == 0){
 				total += i;
+			}
 		}
 		System.out.println(total);
 	}
