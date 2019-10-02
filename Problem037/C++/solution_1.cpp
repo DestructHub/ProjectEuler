471c8706252eebd3038a4fc7dfd3ad56ce5f8952
Naman Gera <43007653+namannimmo10@users.noreply.github.com>
2019-10-02 14:54:42 +0530

    Update solution_1.cpp

--- Problem037/C++/solution_1.cpp
+++ Problem037/C++/solution_1.cpp
@@ -2,41 +2,48 @@
 using namespace std;
 
 const int much = 1000000;
-bool isPrime [much];
+bool isPrime[much];
 
-void sieve (){
+void sieve(){
 	memset (isPrime, true, sizeof(isPrime));
 	isPrime[0] = isPrime [1] = false;
-	for (int i = 2; i < much; ++i)
-		for (int j = (i << 1); j < much; j += i)
+	for (int i = 2; i < much; ++i){
+		for (int j = (i << 1); j < much; j += i){
 			isPrime [j] = false;
+		}
+	}
 }
 
-bool test (int t)
+bool test(int t)
 {
 	int orig = t, cnt = 0, tmp10 = 1;
 	while (t > 0){
-		if (!isPrime [t])
+		if (!isPrime [t]){
 			return false;
+		}
 		t /= 10;
-		++cnt, tmp10 *= 10;
+		++cnt;
+		tmp10 *= 10;
 	}
-	while (orig > 0)
+	while(orig > 0)
 	{
-		if (!isPrime[orig])
+		if (!isPrime[orig]){
 			return false;
+		}
 		orig = (orig % (tmp10 /= 10));
 	}
 	return true;
 }
 
-int main ()
+int main()
 {
 	long long sum = 0;
 	sieve ();
-	for (int i = 10; i < much; ++i)
-		if (test(i))
+	for (int i = 10; i < much; ++i){
+		if (test(i)){
 			sum += i;
+		}
+	}
 	cout << sum << '\n';
 	return 0;
 }
