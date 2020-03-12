80c1ed69ec8d25953d9fada971acd2a11b980894
Manoel Vilela <manoel.neto@neoway.com.br>
2020-03-11 21:16:43 -0300

    Problem054: Fix p054_poker.txt path at solution_1.py

--- Problem054/Python/solution_1.py
+++ Problem054/Python/solution_1.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-hands = [line.split(' ') for line in open('p054_poker.txt')]
+hands = [line.split(' ') for line in open('../p054_poker.txt')]
 
 values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 straights = [(v, v - 1, v - 2, v - 3, v - 4) for v in range(14, 5, -1)]
